daaa7c5a74314244502cb3d2195fea857075744d
scawp <rob@scawp.com>
2022-06-16 23:50:08 +0200

    no longer needs to be ran inside the steam dir, added a way to generate an appmanifest

--- steamGetManifest.sh
+++ steamGetManifest.sh
@@ -1,36 +1,47 @@
 #!/bin/bash
 
-if [ ! -d "$1" ]; then
-  echo "please run this command with the path where you copied your games from eg \"./steamGetManifest.sh run/media/deck/external/steamLibrary/steamapps\""
+steamapps_dir="/home/deck/.local/share/Steam/steamapps" #TODO assuming you are running on a Steam Deck
+tmp_dir="$(dirname $(realpath "$0"))/tmp"
+template="$(dirname $(realpath "$0"))/template/appmanifest_TEMPLATE.acf"
+
+echo "$steamapps_dir/common/"
+mkdir -p "$tmp_dir"
+find $steamapps_dir/common/* -maxdepth 0 -type d | sed 's/^.*\/common\///g' > "$tmp_dir/results.txt"
+
+if [ ! -s "$tmp_dir/results.txt" ]; then
+  echo "no results, Quitting. Are you running this script in \"steamapps\"?"
   exit 1;
 fi
 
-mkdir -p tmp
-find ./common/* -maxdepth 0 -type d | sed 's/.\/common\///g' > tmp/results.txt
-#true > tmp/results.txt
-if [ ! -s tmp/results.txt ]; then
-  echo "no results, are you running this script in \"steamapps\"?"
-  exit 1;
-fi
-
-find ./common/* -maxdepth 0 -type d | sed 's/.\/common\//^\\s*\\"installdir\\"\\s*\\"/g' | sed 's/$/\\"\$/' > tmp/regex.txt
-grep -hof tmp/regex.txt appmanifest_*.acf | sed 's/\s*\"installdir\"\s*\"//g' | sed 's/\"$//g' > tmp/found.txt
+find $steamapps_dir/common/* -maxdepth 0 -type d | sed 's/^.*\/common\//^\\s*\\"installdir\\"\\s*\\"/g' | sed 's/$/\\"\$/' > "$tmp_dir/regex.txt"
+grep -hof $tmp_dir/regex.txt $steamapps_dir/appmanifest_*.acf | sed 's/\s*\"installdir\"\s*\"//g' | sed 's/\"$//g' > "$tmp_dir/found.txt"
 
 echo "The following games are missing Appmanifests:"
 
-grep -vFf tmp/found.txt tmp/results.txt | tee tmp/missing.txt
+grep -vFf $tmp_dir/found.txt $tmp_dir/results.txt | tee $tmp_dir/missing.txt
 
-echo "Open tmp/missing.txt and remove any games you DON'T want to import an Appmanifest. Press ENTER to continue or ctrl+c to exit"
+echo "Open $tmp_dir/missing.txt and remove any games you DON'T want to import an Appmanifest. Press ENTER to continue or ctrl+c to exit"
 read
 echo "lets go!"
 
-sed 's/^/\\"installdir\\"\\s*\\"/g' tmp/missing.txt  | sed 's/$/\\"\$/' > tmp/findreg.txt
-grep -lf tmp/findreg.txt $1/appmanifest_*.acf | sed 's/\s*\"installdir\"\s*\"//g' | sed 's/\"$//g' | tee tmp/tocopy.txt
+if [ ! -d "$1" ]; then
+  echo "Source dir not provided, manually add the AppId to missing tab seperated. ENTER to continue manually or ctrl+c to QUIT"
 
-while read -r line; do
-  cp "$line" .
-done < tmp/tocopy.txt
+  while read -r line; do
+    IFS=$'\t'; column=($line); unset IFS; #0=INSTALLDIR,1=APPID
+    echo "${column[1]} --- ${column[0]}"
+    sed -e "s/\[APPID\]/${column[1]}/g" -e "s/\[INSTALLDIR\]/${column[0]}/g"  $template > $tmp_dir/appmanifest_${column[1]}.acf
+    #sed "s/\[INSTALLDIR\]/${column[0]}/g" $tmp_dir/appmanifest_${column[1]}.acf > $tmp_dir/a3ppmanifest_${column[1]}.acf
+  done < "$tmp_dir/missing.txt"
+  
+else
+  sed 's/^/\\"installdir\\"\\s*\\"/g' $tmp_dir/missing.txt  | sed 's/$/\\"\$/' > $tmp_dir/findreg.txt
+  grep -lf $tmp_dir/findreg.txt $1/appmanifest_*.acf | sed 's/\s*\"installdir\"\s*\"//g' | sed 's/\"$//g' | tee $tmp_dir/tocopy.txt
 
-echo "Done! (feel free to delete \"tmp\")"
+  while read -r line; do
+    cp "$line" "$steamapps_dir"
+  done < "$tmp_dir/tocopy.txt"
+fi
+echo "Done!"
 
 exit 0;
